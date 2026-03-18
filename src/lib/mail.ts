4ef5a7fbcb90d7e9caa6d9c88265d0054d85541f
kanchan sharma <kanusharma881388@gmail.com>
2026-03-19 00:46:19 +0530

    fix: disable resend click tracking issues and use env for domain url

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,10 +1,10 @@
 
 import { Resend } from "resend";
-
+const domain = process.env.NEXTAUTH_URL;
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/new-verification?token=${token}`;
+  const confirmLink = `${domain}/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
