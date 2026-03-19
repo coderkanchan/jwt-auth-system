b80f84315e869ddb540c31b271b7bb8796274c43
kanchan sharma <kanusharma881388@gmail.com>
2026-03-20 00:08:43 +0530

    chore: add currentUser helper for secure server-side session access

---
+++ src/lib/auth-user.ts
@@ -0,0 +1,8 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth"; 
+
+export const currentUser = async () => {
+  const session = await getServerSession(authOptions);
+
+  return session?.user;
+};
