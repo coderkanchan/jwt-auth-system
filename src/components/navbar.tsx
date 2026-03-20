beeff8003fc1573f9367e0f745f9e1e30c568a5d
kanchan sharma <kanusharma881388@gmail.com>
2026-03-20 14:57:50 +0530

    fix: force case sensitive rename for Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { LogoutButton } from "./auth/logout-button";
 
+
 export const Navbar = async () => {
   const session = await getServerSession(authOptions);
 
